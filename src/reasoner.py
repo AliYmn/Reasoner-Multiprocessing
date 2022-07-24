f430ca4e43afb804904c35a517858fd59b065c46
aliyaman <aliymnmonotech@gmail.com>
2022-07-24 22:29:48 +0300

    algorithm was created.

--- src/reasoner.py
+++ src/reasoner.py
@@ -1,10 +1,22 @@
-from http.client import IM_USED
-from owlready2 import get_ontology,Imp, sync_reasoner,sync_reasoner_hermit,sync_reasoner_pellet,destroy_entity
+from owlready2 import (
+    Imp,
+    destroy_entity,
+    get_ontology,
+    sync_reasoner,
+    sync_reasoner_hermit,
+    sync_reasoner_pellet,
+)
 
 
 class Reasoner:
-    def __init__(self, file_name: str, reasoner_type: str, sleep_time: int, 
-                infer_property_values : bool, infer_data_property_values : bool) -> None:
+    def __init__(
+        self,
+        file_name: str,
+        reasoner_type: str,
+        sleep_time: int,
+        infer_property_values: bool,
+        infer_data_property_values: bool,
+    ):
         self.file_name = file_name
         self.reasoner_type = reasoner_type
         self.sleep_time = sleep_time
@@ -13,44 +25,53 @@ class Reasoner:
 
     def ontoLoad(self) -> object:
         try:
-            onto = get_ontology(self.file_name).load()
-            return onto
+            return get_ontology(self.file_name).load()
         except FileNotFoundError:
-            print("The specified file was not found in the location. '{file_name}'".format(file_name=self.file_name))
+            print("The specified file was not found in the location.'{file_name}'".format(file_name=self.file_name))
 
-    def deleteAllRules(self) -> None:
-        for rule in self.ontoLoad.rules():
-            destroy_entity(rule)
-        self.ontoLoad.save()
-
-    def getRules(self) -> object:
-        rules = []
-        rule = self.ontoLoad().rules()
-        if len(list(rule)) == 0:
-            raise Exception("There not found any rule.")
-        else:
-            for r in rule:
-                rules.append(r)
-            self.deleteAllRules()
-            return rule 
+    def saveOnto(self) -> None:
+        onto = get_ontology(self.file_name)
+        onto.save(self.file_name)
 
     def reasonerType(self) -> object:
         if self.reasoner_type == "pellet":
-            return sync_reasoner_pellet(infer_property_values = True, infer_data_property_values = True)
+            return sync_reasoner_pellet(infer_property_values=True, infer_data_property_values=True)
         elif self.reasoner_type == "hermit":
             return sync_reasoner_hermit(infer_property_values=self.infer_property_values)
         else:
-            return sync_reasoner(infer_property_values = self.infer_property_values, infer_data_property_values = self.infer_data_property_values)
-        
-    def run(self):
+            return sync_reasoner(
+                infer_property_values=self.infer_property_values,
+                infer_data_property_values=self.infer_data_property_values,
+            )
+
+    def deleteAllRules(self) -> None:
+        for rule in self.ontoLoad().rules():
+            destroy_entity(rule)
+        self.saveOnto()
+
+    def getRules(self) -> object:
+        rules = []
+        for rule in self.ontoLoad().rules():
+            rules.append(rule)
+
+        return rules
+
+    def insertRules(self, rules) -> None:
         with self.ontoLoad():
             reasonerImp = Imp()
-            for rule in self.getRules():
-                reasonerImp.set_as_rule("""{rule_syntax}""".format(rule_syntax=rule))
-                self.ontoLoad().save()
+            for rule in rules:
+                reasonerImp.set_as_rule("""{rule}""".format(rule=rule))
+                self.saveOnto()
+
+    def run(self) -> None:
+        rules = self.getRules()
+        self.deleteAllRules()
+        with self.ontoLoad():
+            reasonerImp = Imp()
+            for rule in rules:
+                reasonerImp.set_as_rule("""{rule}""".format(rule=rule))
+                self.saveOnto()
                 self.reasonerType()
-            self.deleteAllRules()
-            
-# if __name__ == '__main__':
-#     reasoner = Reasoner(file_name="dsm.owl", reasoner_type=" ")
-#     print('reasoner: ', reasoner)
+                self.deleteAllRules()
+
+        self.insertRules(rules)
